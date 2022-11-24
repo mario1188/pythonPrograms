f0938885ec5dedbe821b64bccb7ebe4401acd739
mario serna <a252616@alumnos.uaslp.mx>
2022-11-24 09:22:21 -0600

    section 1 over

--- section1_variables_and_numbers.py
+++ section1_variables_and_numbers.py
@@ -11,6 +11,10 @@ print(suma)
 decimalNumber = 29.38
 print(decimalNumber)
 
+decimal = number + decimalNumber
+print(type(decimal), " ", decimal)
+
+
 """concatenate strings"""
 cad = "hola mundo"
 print(cad)
@@ -32,3 +36,44 @@ print(type(cadNumber), " ", cadNumber)
 
 stringToInt = int(cadNumber)
 print(type(stringToInt), " ", stringToInt)
+
+floatTostring = 101.23
+cadFloat = str(floatTostring)
+print(type(cadFloat), " ", cadFloat)
+strToFloat = float(cadFloat)
+print(type(strToFloat), " ", strToFloat)
+
+"""
+EJERCICIO 1
+-create a variable "number1" with the value 5, display the type of the variable
+-create a variable "number2" with the value 6.5, display the type of the variable
+-create a variable "number3" with the value 7, display the type of the variable
+-create a variable "suma" wich contains the value of the sum of the other three variables
+ show the result and display the type of the varaible
+"""
+print("----ejercicio 1----")
+number1 = 5
+print(type(number1))
+number2 = 6.5
+print(type(number2))
+number3 = 7
+print(type(number3))
+suma = number1+number2+number3
+print("suma: ", suma)
+print(type(suma))
+print("--------------------")
+
+"""
+EJERCICIO 2
+-create a variable "number1" with the value 5
+-create a variable "number2" with the value 3
+-create a variable "suma" wich contains the value of the sum of the other two variables
+-convert the variable "suma" to string and name it "strSuma"
+-create a varaible "resultado" that concatenate the text "el resultado es " and the varaible strSuma
+"""
+number1 = 5
+number2 = 3
+suma = number1 + number2
+strSuma = str(suma)
+resultado = "el resultado es " + strSuma
+print(resultado)
